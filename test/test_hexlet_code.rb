fbfb95ccf5780c996643faaf5e95f914670e04a9
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2024-03-17 19:14:49 +0400

    add tests

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,11 +3,15 @@
 require 'test_helper'
 
 class TestHexletCode < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::HexletCode::VERSION
-  end
+  User = Struct.new(:name, :job, keyword_init: true)
 
-  def test_it_does_something_useful
-    assert true
+  def test_form_generate
+    user = User.new name: 'rob'
+
+    form_with_url = HexletCode.form_for user, url: '/users'
+    assert(form_with_url == '<form action="/users" method="post"></form>')
+
+    form_without_url = HexletCode.form_for user
+    assert(form_without_url == '<form action="#" method="post"></form>')
   end
 end
